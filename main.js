f37142c1e465961100c3fda8fd9223dfb8bd1d3c
Enzocom811 <enzocom811@gmail.com>
2022-08-20 23:44:19 -0300

    js

--- main.js
+++ main.js
@@ -2,3 +2,32 @@
 
 let usuario = ""
 let visitas = 0
+
+const $parrafo = document.getElementById ("paragraph")
+const $cuentaVisitas = document.getElementById ("contador")
+
+$parrafo.addEventListener("click", (e) => nombreUsuario(e))
+
+function nombreUsuario(e) {
+    let name = prompt ("Ingresá tu nombre");
+    let nameId = usuario = name;
+    localStorage.setItem("user", nameId);
+    location.reload();
+};
+
+function getValue() {
+    let value = localStorage.getItem("user");
+    $parrafo.innerHTML = `Bienvenid@ ${value || null || ""}`
+};
+
+getValue()
+
+let contador =+ localStorage.getItem("visitas") || 0;
+
+function visitCounter() {
+    contador = contador + 1;
+    localStorage.setItem("visitas", contador)
+    $cuentaVisitas.innerHTML = `Visitas: ${contador}`
+}
+
+visitCounter()
